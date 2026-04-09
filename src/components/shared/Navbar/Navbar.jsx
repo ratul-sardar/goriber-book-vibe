8e9498e8c6bfacaa8cdbfac707517e30762761e4
ratul-sardar <ratulsardar4747@gmail.com>
2026-04-09 23:14:01 +0600

    nav is built but need to refine it more

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // Toggle Hamburger menu
+  function handleMenu() {
+    setMobileMenuOpen(!mobileMenuOpen);
+  }
+
   return (
-    <nav className="w-11/12 py-6 container mx-auto flex justify-between items-center gap-8">
+    <nav className="relative w-11/12 py-6 container mx-auto flex justify-between items-center gap-8">
       {/* Brand Logo */}
       <div className="min-w-fit">
         <h4 className="text-xl font-bold">Brand Logo</h4>
@@ -39,9 +47,41 @@ const Navbar = () => {
 
       {/* Hamburger Menu Trigger */}
 
-      <div className="lg:hidden text-3xl">
-        <HiOutlineMenuAlt1 />
+      <div onClick={handleMenu} className="lg:hidden text-3xl">
+        {mobileMenuOpen ? <IoClose /> : <HiOutlineMenuAlt1 />}
       </div>
+
+      {/* Mobile Menu */}
+      {mobileMenuOpen && (
+        <div className="absolute top-full left-0 w-screen bg-white  ">
+          <div className="w-full flex flex-col items-center gap-8">
+            {/* Site Links */}
+            <ul className="w-full space-y-4">
+              <li>
+                <a href="#" className="hover:text-blue-600">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-blue-600">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-blue-600">
+                  More
+                </a>
+              </li>
+            </ul>
+
+            {/* Cta */}
+            <div className="min-w-fit space-x-3">
+              <button className="btn">Login</button>
+              <button className="btn btn-primary">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
